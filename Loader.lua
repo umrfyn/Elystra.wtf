a7827284ca4f4fe0fdc7828647eab4f08e76bcae
umrfyn <100963276+umrfyn@users.noreply.github.com>
2026-03-13 16:27:02 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -3,7 +3,8 @@ local PlaceScripts = {
     [77837537595343] = "https://raw.githubusercontent.com/4levy/Elystra.wtf/refs/heads/main/BannaTown.lua",
     [114116662845070] = "https://raw.githubusercontent.com/4levy/Elystra.wtf/refs/heads/main/CityBanna.lua",
     [14807448145] = "https://raw.githubusercontent.com/umrfyn/Elystra.wtf/refs/heads/main/ThaiBanCity.txt",
-    [103854444055060] = "https://raw.githubusercontent.com/umrfyn/Elystra.wtf/refs/heads/main/SilentAssassins.lua"
+    [103854444055060] = "https://raw.githubusercontent.com/umrfyn/Elystra.wtf/refs/heads/main/SilentAssassins.lua",
+    [89528376983973] = "https://raw.githubusercontent.com/umrfyn/Elystra.wtf/refs/heads/main/MazePVPOPENSRC.lua"
 }
 
 local Players = game:GetService("Players")
